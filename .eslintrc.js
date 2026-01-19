6018ac3d58c951f50034dfaea29f66e013ca54d9
minhducant <minhduc.gco@gmail.com>
2026-01-19 17:23:58 +0700

    Enhance: integrate Prettier for code formatting, add ESLint configurations, and include react-native-keychain dependency

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,11 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    '@react-native',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['prettier'],
   rules: {
-    'prettier/prettier': ['error', {endOfLine: 'auto'}],
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
   },
 };
